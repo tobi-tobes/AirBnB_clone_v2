32243ed48a407fb95898b01c8fa8a2a6543ccf5a
Tobi Tijani <tobi_tijani@yahoo.com>
2023-06-20 10:01:26 -0700

    Tasks 7 and 8

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state',
-                          cascade='all, delete, delete-orphan')
+                          cascade='all, delete')
 
     @property
     def cities(self):
--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from models import storage_type
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -9,13 +8,8 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    if storage_type == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', cascade="all, delete", backref="user")
