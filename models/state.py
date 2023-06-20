d609941c269f9669748ff12e2e2ef2d7b4258425
Pheyi32 <feyisogoadeyemo.com>
2023-06-20 04:30:38 -0700

    task 6

--- models/state.py
+++ models/state.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models import storage_type
-from sqlalchemy import Column, String, ForeignKey
+from models.city import City
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
+class State(BaseModel, Base):
+    """ State class / table model"""
+    __tablename__ = 'states'
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities',
+        cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
     else:
         name = ''
-        state_id = ''
+
+        @property
+        def cities(self):
+            '''returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
