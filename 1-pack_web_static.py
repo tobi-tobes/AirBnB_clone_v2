2edb08c8c2e2dc2983cce0b76dc0259b1eada05a
Tobi Tijani <tobi_tijani@yahoo.com>
2023-07-06 11:27:08 -0700

    Fixed path

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -19,10 +19,10 @@ def do_pack():
     local("mkdir -p versions")
 
     current = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    fname = "versions/web_static_{}.tgz".format(current)
+    fname = "./versions/web_static_{}.tgz".format(current)
 
     result = local("tar -cvzf {} web_static".format(fname))
-    if result.succeeded and os.path.isfile(fname) and result.return_code == 0:
+    if result.succeeded:
         return fname
     else:
         return None
