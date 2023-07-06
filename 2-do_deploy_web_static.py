454533a8b12092122f79468a03a4939179094398
Tobi Tijani <tobi_tijani@yahoo.com>
2023-07-06 11:59:51 -0700

    Revert "Task 2"
    This reverts commit 00f9291b68cbeb67490cb72812839ad8a089016c.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,60 +1,85 @@
 #!/usr/bin/python3
 """
-Script generates a .tgz archive from web_static folder
+2-do_deploy_web_static.py
+This module contains a Fabric script that distributes an archive
+to your web servers, using the function do_deploy
 """
-from fabric.operations import local, run, put, env
-from datetime import datetime
+
+from fabric.api import *
 import os
+import datetime
 
-
-env.hosts = ['35.237.142.70', '35.231.108.250']
-env.user = 'ubuntu'
+env.hosts = ['54.160.124.170', '52.205.97.123']
+env.user = "ubuntu"
 
 
 def do_pack():
     """
-    function creates a .tgz archive
+    This function generates a .tgz archive from the contents
+    of the web_static folder of your AirBnB Clone repo
     """
-
-    name = "./versions/web_static_{}.tgz"
-    name = name.format(datetime.now().strftime("%Y%m%d%H%M%S"))
     local("mkdir -p versions")
-    create = local("tar -cvzf {} web_static".format(name))
-    if create.succeeded:
-        return name
+
+    current = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    fname = "./versions/web_static_{}.tgz".format(current)
+
+    result = local("tar -cvzf {} web_static".format(fname))
+    if result.succeeded:
+        return fname
     else:
         return None
 
 
 def do_deploy(archive_path):
     """
-    function to dist to web server
+    This function distributes an archive to your web servers
     """
-
     if not os.path.exists(archive_path):
         return False
-    if not put(archive_path, "/tmp/").succeeded:
+    if not os.path.isfile(archive_path):
         return False
-    print("Hello")
-    filename = archive_path[9:]
-    foldername = "/data/web_static/releases/" + filename[:-4]
-    filename = "/tmp/" + filename
-    if not run('mkdir -p {}'.format(foldername)).succeeded:
+    if ".tgz" not in archive_path:
         return False
-    if not run('tar -xzf {} -C {}'.format(filename, foldername)).succeeded:
-        return False
-    if not run('rm {}'.format(filename)).succeeded:
-        return False
-    if not run('mv {}/web_static/* {}'.format(foldername,
-                                              foldername)).succeeded:
-        return False
-    if not run('rm -rf {}/web_static'.format(foldername)).succeeded:
-        return False
-    if not run('rm -rf /data/web_static/current').succeeded:
-        return False
-    return run('ln -s {} /data/web_static/current'.format(
-        foldername)).succeeded
 
+    result = put(archive_path, "/tmp/")
+    if result.failed or result.return_code != 0:
+        return False
 
-if __name__ == "__main__":
-    do_pack()
+    files = archive_path.split("/")
+    file_with_ext = files[-1]
+    filename = files[-1].split(".")[0]
+
+    result = run("mkdir -p /data/web_static/releases/{}/".format(filename))
+    if result.failed or result.return_code != 0:
+        return False
+
+    result = run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+               format(file_with_ext, filename))
+    if result.failed or result.return_code != 0:
+        return False
+
+    result = run("rm /tmp/{}".format(file_with_ext))
+    if result.failed or result.return_code != 0:
+        return False
+
+    result = run("mv /data/web_static/releases/{}/web_static/* \
+/data/web_static/releases/{}/".format(filename, filename))
+    if result.failed or result.return_code != 0:
+        return False
+
+    result = run("rm -rf /data/web_static/releases/{}/web_static/".
+               format(filename))
+    if result.failed or result.return_code != 0:
+        return False
+
+    result = run("rm -rf /data/web_static/current")
+    if result.failed or result.return_code != 0:
+        return False
+
+    result = run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+               format(filename))
+    if result.failed or result.return_code != 0:
+        return False
+
+    print("New version deployed!")
+    return True
