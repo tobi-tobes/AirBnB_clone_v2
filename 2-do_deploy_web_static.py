582c143f23fac92fc565250ba9e1831f5d7ed591
Tobi Tijani <tobi_tijani@yahoo.com>
2023-07-06 12:44:56 -0700

    Task 2 fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -37,7 +37,7 @@ def do_deploy(archive_path):
         return False
 
     result = run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-               format(file_with_ext, filename))
+                 format(file_with_ext, filename))
     if result.failed or result.return_code != 0:
         return False
 
@@ -51,7 +51,7 @@ def do_deploy(archive_path):
         return False
 
     result = run("rm -rf /data/web_static/releases/{}/web_static/".
-               format(filename))
+                 format(filename))
     if result.failed or result.return_code != 0:
         return False
 
@@ -59,8 +59,8 @@ def do_deploy(archive_path):
     if result.failed or result.return_code != 0:
         return False
 
-    result = run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-               format(filename))
+    result = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+                 .format(filename))
     if result.failed or result.return_code != 0:
         return False
 
