862dc7086bf6f0a6a347b08b66fa67b1ac2c9d5b
Tobi Tijani <tobi_tijani@yahoo.com>
2023-07-07 02:58:24 -0700

    Fixed Pycodestyle issues for Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,8 +23,6 @@ def do_deploy(archive_path):
         return False
     if not os.path.isfile(archive_path):
         return False
-    if ".tgz" not in archive_path:
-        return False
 
     filename = os.path.basename(archive_path)
     filename_no_ext = os.path.splitext(filename)[0]
@@ -32,19 +30,19 @@ def do_deploy(archive_path):
     try:
         put(archive_path, "/tmp/")
         run("sudo mkdir -p /data/web_static/releases/{}/"
-                .format(filename_no_ext))
+            .format(filename_no_ext))
         run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
             format(filename, filename_no_ext))
         run("sudo rm /tmp/{}".format(filename))
-        run("sudo mv /data/web_static/releases/{}/web_static/* \
-/data/web_static/releases/{}/".format(filename_no_ext, filename_no_ext))
+        run("sudo mv /data/web_static/releases/{}/web_static/* /data\
+/web_static/releases/{}/".format(filename_no_ext, filename_no_ext))
         run("sudo rm -rf /data/web_static/releases/{}/web_static".
             format(filename_no_ext))
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
-            format(filename_no_ext))
+        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(filename_no_ext))
         print("New version deployed!")
-        return True
-
     except Exception as e:
         return False
+
+    return True
