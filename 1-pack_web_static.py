429171f6589aed722bbec506fc2ba60539164b2c
Tobi Tijani <tobi_tijani@yahoo.com>
2023-07-07 01:14:18 -0700

    Task 1:

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -22,7 +22,7 @@ def do_pack():
 
     fname = "versions/web_static_{}.tgz".format(current)
 
-    result = local("tar -cvzf {} web_static/*".format(fname))
+    result = local("tar -cvzf {} ./web_static".format(fname))
 
     if result.succeeded:
         file_stats = os.stat(fname)
