a1330e27e11295afc48ac75565c236b3a8a0e776
Tobi Tijani <tobi_tijani@yahoo.com>
2023-07-07 02:44:25 -0700

    Task 2: Added sudo for commands

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,6 +11,8 @@ import datetime
 
 env.hosts = ['54.160.124.170', '52.205.97.123']
 env.user = "ubuntu"
+env.key_filename = "/alx-system_engineering-devops/\
+        0x04-loops_conditions_and_parsing/0-RSA_public_key"
 
 
 def do_deploy(archive_path):
@@ -29,16 +31,17 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(filename_no_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+        run("sudo mkdir -p /data/web_static/releases/{}/"
+                .format(filename_no_ext))
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
             format(filename, filename_no_ext))
-        run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* \
+        run("sudo rm /tmp/{}".format(filename))
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
 /data/web_static/releases/{}/".format(filename_no_ext, filename_no_ext))
-        run("rm -rf /data/web_static/releases/{}/web_static".
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".
             format(filename_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
             format(filename_no_ext))
         print("New version deployed!")
         return True
